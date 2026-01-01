93da94e38a7927777ea6c6d141f836343a49f96f
Axel C <axel.croize@hotmail.fr>
2026-01-01 20:35:15 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   publicDir: '../public/',
   base: './',
   build: {
-    outDir: 'build',
+    outDir: '../build',
     emptyOutDir: true, // Empty the folder first
     sourcemap: true // Add sourcemap
   },
